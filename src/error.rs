e27b1a47eaadc153a8fe6343259446664a7b3ad4
bagmeg <42.4.joonpark@gmail.com>
2022-02-04 18:24:44 +0900

    Added my_authorize(), test. change name later

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,13 @@
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum SessionError {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    UrlParseError(#[from] url::ParseError),
+    #[error("Error: NoneError")]
+    NoneError,
+    #[error("Error: Server Unauthorized")]
+    ServerUnauthorized,
+}
