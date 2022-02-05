9a44252450572b5262498f9e57dc2f40c3af6846
bagmeg <42.4.joonpark@gmail.com>
2022-02-05 18:19:15 +0900

    read id, secret from config.toml & add member functions

--- src/authorize.rs
+++ src/authorize.rs
@@ -5,32 +5,41 @@ use oauth2::{
     AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope,
     TokenResponse, TokenUrl,
 };
+use serde::Deserialize;
+use std::fs;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 use url::Url;
 
 use crate::error::SessionError;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, Deserialize)]
 pub struct Session {
     client_id: String,
     client_secret: String,
+    access_token: Option<String>,
 }
 
 impl Session {
     pub fn new() -> Result<Self, SessionError> {
-        // TODO
-        // read config file and set client_id and client_secret
-        Ok(Session {
-            client_id: String::from(""),
-            client_secret: String::from(""),
-        })
+        let path = "./config.toml";
+        let content = fs::read_to_string(path)?;
+        Ok(toml::from_str(&content)?)
     }
     pub fn get_client_id(&self) -> &str {
         self.client_id.as_str()
     }
     pub fn get_client_secret(&self) -> &str {
         self.client_secret.as_str()
+    }
+    pub fn get_access_token(&self) -> Option<String> {
+        self.access_token.clone()
+    }
+    pub fn set_access_token(&mut self, token: String) {
+        self.access_token = Some(token);
+    }
+    pub fn update_access_token(&mut self, token: String) {
+        self.access_token = Some(token);
     }
 }
 
